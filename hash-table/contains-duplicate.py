53a025378ad8a262d366e3b6960f07bf1a47550b
Sen Feng <87135268+SenF1@users.noreply.github.com>
2024-05-23 12:13:11 -0400

    LC-contains-duplicate [Runtime: 405 ms; Memory: 31.8 MB]

---
+++ hash-table/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        lst = set()
+        for num in nums:
+            if num in lst:
+                return True
+            lst.add(num)
+
+        return False
+        
