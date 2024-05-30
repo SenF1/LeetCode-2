aa12ec9a49c150f3a192fca40e1a39f15b7eeaab
Sen Feng <87135268+SenF1@users.noreply.github.com>
2024-05-30 07:56:32 -0400

    LC-valid-anagram [Runtime: 54 ms; Memory: 16.9 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,14 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dic = defaultdict(int)
-        for v in s:
-            dic[v] += 1
+        if len(s) != len(t): return False
+
+        sd = dict()
+        td = dict()
+        for c in s:
+            if c in sd:
+                sd[c] += 1
+            else:
+                sd[c] = 1
         
-        for v in t:
-            dic[v] -= 1
+        for c in t:
+            if c in td:
+                td[c] += 1
+            else:
+                td[c] = 1
         
-        for value in dic.values():
-            if value != 0:
+        for key in sd:
+            if key not in td or sd[key] != td[key]:
                 return False
+        
         return True
+
         
