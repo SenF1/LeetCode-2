a0bd96c0affc79e0d7c1788f83da1fafd1fe17d8
Sen Feng <87135268+SenF1@users.noreply.github.com>
2024-06-03 10:49:57 -0400

    LC-top-k-frequent-elements [Runtime: 92 ms; Memory: 21.9 MB]

--- array/top-k-frequent-elements.py
+++ array/top-k-frequent-elements.py
@@ -1,9 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = dict()
+        count = {}
+        freq = [[] for _ in range(len(nums) + 1)]
+        res = []
+
         for num in nums:
-            d[num] = 1 + d.get(num, 0)
+            count[num] = 1 + count.get(num, 0)
         
-        d = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
-        result = list(d.keys())[:k]
-        return result       
+        for key, value in count.items():
+            freq[value].append(key)
+        
+        for i in range(len(freq)-1, -1, -1):
+            for v in freq[i]:
+                res.append(v)
+
+                if len(res) == k:
+                    return res
+
+        return res       
