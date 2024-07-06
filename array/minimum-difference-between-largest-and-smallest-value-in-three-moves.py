19991848b36cec84360f0b2f4c0beb164b8cd1f7
Sen Feng <87135268+SenF1@users.noreply.github.com>
2024-07-06 09:24:17 -0400

    LC-minimum-difference-between-largest-and-smallest-value-in-three-moves [Runtime: 267 ms; Memory: 28 MB]

--- array/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ array/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,8 +1,7 @@
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
         n = len(nums)
-        if n <= 4:
-            return 0
+        if n <= 4: return 0
         nums.sort()
 
         min_diff = min(
